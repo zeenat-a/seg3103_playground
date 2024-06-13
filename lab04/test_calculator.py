5d8a964572b23134be30824a676ed568fd0240aa
Aparna Kagini <akagi048@uottawa.ca>
2024-06-13 16:19:28 -0400

    Adding failing tests

---
+++ lab04/test_calculator.py
@@ -0,0 +1,23 @@
+import pytest
+from calculator import Calculator
+
+def test_addition():
+    calc = Calculator()
+    assert calc.add(2, 3) == 5
+
+def test_subtraction():
+    calc = Calculator()
+    assert calc.subtract(5, 3) == 2
+
+def test_multiplication():
+    calc = Calculator()
+    assert calc.multiply(2, 3) == 6
+
+def test_division():
+    calc = Calculator()
+    assert calc.divide(6, 3) == 2
+
+def test_division_by_zero():
+    calc = Calculator()
+    with pytest.raises(ZeroDivisionError):
+        calc.divide(6, 0)
