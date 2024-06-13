51509599a600f5d81a5e464fd59801aea50f45c1
Aparna Kagini <akagi048@uottawa.ca>
2024-06-13 16:42:54 -0400

    Refactoring code to pass all tests

--- lab04/simple_calculator.py
+++ lab04/simple_calculator.py
@@ -1,4 +1,17 @@
 class Calculator:
     def add(self, a, b):
         return a + b
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("division by zero")
+        return a / b
+    
+    def power(self, base, exponent):
+        return base ** exponent
     
