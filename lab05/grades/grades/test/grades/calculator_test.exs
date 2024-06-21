ca450cc20e4c6e15c17e8552b7def6fc1f2d6e7a
zeenat-a <zeenatanjum2004@gmail.com>
2024-06-20 22:51:50 -0400

    application starts

---
+++ lab05/grades/grades/test/grades/calculator_test.exs
@@ -0,0 +1,16 @@
+defmodule Grades.CalculatorTest do
+  use ExUnit.Case
+  alias Grades.Calculator
+
+  describe "percentage_grade/1" do
+    test "sample" do
+      assert 85 ==
+               Calculator.percentage_grade(%{
+                 homework: [0.8],
+                 labs: [1, 1, 1],
+                 midterm: 0.70,
+                 final: 0.9
+               })
+    end
+  end
+end
